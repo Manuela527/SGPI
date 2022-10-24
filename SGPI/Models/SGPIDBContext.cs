c0c4603273fbdfb451180b5e073faa4ace9c76a1
Manuela527 <manu52797@hotmail.com>
2022-10-24 14:55:13 -0500

    Creación modelos

---
+++ SGPI/Models/SGPIDBContext.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SGPI.Models
+{
+    public partial class SGPIDBContext : DbContext
+    {
+        public SGPIDBContext()
+        {
+        }
+
+        public SGPIDBContext(DbContextOptions<SGPIDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Asignatura> Asignaturas { get; set; } = null!;
+        public virtual DbSet<Documento> Documentos { get; set; } = null!;
+        public virtual DbSet<Entrevistum> Entrevista { get; set; } = null!;
+        public virtual DbSet<Estudiante> Estudiantes { get; set; } = null!;
+        public virtual DbSet<Genero> Generos { get; set; } = null!;
+        public virtual DbSet<Homologacion> Homologacions { get; set; } = null!;
+        public virtual DbSet<Pago> Pagos { get; set; } = null!;
+        public virtual DbSet<Programa> Programas { get; set; } = null!;
+        public virtual DbSet<Programacion> Programacions { get; set; } = null!;
+        public virtual DbSet<Rol> Rols { get; set; } = null!;
+        public virtual DbSet<TipoHomologacion> TipoHomologacions { get; set; } = null!;
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-2URG37U\\SQLEXPRESS;Database=SGPIDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Asignatura>(entity =>
+            {
+                entity.HasKey(e => e.IdAsignatura)
+                    .HasName("PK__Asignatu__94F174B8C6721293");
+
+                entity.ToTable("Asignatura");
+
+                entity.Property(e => e.IdAsignatura).ValueGeneratedNever();
+
+                entity.Property(e => e.Codigo)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdProgramaNavigation)
+                    .WithMany(p => p.Asignaturas)
+                    .HasForeignKey(d => d.IdPrograma)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Asignatura_Programa");
+            });
+
+            modelBuilder.Entity<Documento>(entity =>
+            {
+                entity.HasKey(e => e.IdDocumento)
+                    .HasName("PK__Document__E5207347BBAA7C7C");
+
+                entity.ToTable("Documento");
+
+                entity.Property(e => e.IdDocumento).ValueGeneratedNever();
+
+                entity.Property(e => e.TipoDocumento)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Entrevistum>(entity =>
+            {
+                entity.HasKey(e => e.IdEntrevista)
+                    .HasName("PK__Entrevis__EE6CE9C7F06C316D");
+
+                entity.Property(e => e.IdEntrevista).ValueGeneratedNever();
+
+                entity.Property(e => e.Fecha).HasColumnType("date");
+
+                entity.HasOne(d => d.IdEstudianteNavigation)
+                    .WithMany(p => p.Entrevista)
+                    .HasForeignKey(d => d.IdEstudiante)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Entrevista_Estudiante");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Entrevista)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Entrevista_Usuario");
+            });
+
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.HasKey(e => e.IdEstudiante)
+                    .HasName("PK__Estudian__B5007C24A14C8576");
+
+                entity.ToTable("Estudiante");
+
+                entity.Property(e => e.IdEstudiante).ValueGeneratedNever();
+
+                entity.Property(e => e.Archivo)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdPagoNavigation)
+                    .WithMany(p => p.Estudiantes)
+                    .HasForeignKey(d => d.IdPago)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Entrevista_Pagos");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Estudiantes)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Estudiante_Usuario");
+            });
+
+            modelBuilder.Entity<Genero>(entity =>
+            {
+                entity.HasKey(e => e.IdGenero)
+                    .HasName("PK__tmp_ms_x__0F8349883F3A5154");
+
+                entity.ToTable("Genero");
+
+                entity.Property(e => e.IdGenero).ValueGeneratedNever();
+
+                entity.Property(e => e.Genero1)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Genero");
+            });
+
+            modelBuilder.Entity<Homologacion>(entity =>
+            {
+                entity.HasKey(e => e.IdHomologacion)
+                    .HasName("PK__Homologa__C774631985E508F0");
+
+                entity.ToTable("Homologacion");
+
+                entity.Property(e => e.IdHomologacion).ValueGeneratedNever();
+
+                entity.Property(e => e.CodigoHomologacion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomAsigHomologacion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PeriodoAcademico)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdAsignaturaNavigation)
+                    .WithMany(p => p.Homologacions)
+                    .HasForeignKey(d => d.IdAsignatura)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Homologacion_Asignatura");
+
+                entity.HasOne(d => d.IdEstudianteNavigation)
+                    .WithMany(p => p.Homologacions)
+                    .HasForeignKey(d => d.IdEstudiante)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Homologacion_Estudiante");
+
+                entity.HasOne(d => d.IdProgramaNavigation)
+                    .WithMany(p => p.Homologacions)
+                    .HasForeignKey(d => d.IdPrograma)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Homologacion_Programa");
+
+                entity.HasOne(d => d.IdTipoHomologacionNavigation)
+                    .WithMany(p => p.Homologacions)
+                    .HasForeignKey(d => d.IdTipoHomologacion)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Homologacion_TipoHomologacion");
+            });
+
+            modelBuilder.Entity<Pago>(entity =>
+            {
+                entity.HasKey(e => e.IdPago)
+                    .HasName("PK__Pagos__FC851A3ACA6B0BBA");
+
+                entity.Property(e => e.IdPago).ValueGeneratedNever();
+
+                entity.Property(e => e.Fecha).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<Programa>(entity =>
+            {
+                entity.HasKey(e => e.IdPrograma)
+                    .HasName("PK__tmp_ms_x__AF94ECA5432089CF");
+
+                entity.ToTable("Programa");
+
+                entity.Property(e => e.IdPrograma).ValueGeneratedNever();
+
+                entity.Property(e => e.Programa1)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("Programa");
+            });
+
+            modelBuilder.Entity<Programacion>(entity =>
+            {
+                entity.HasKey(e => e.IdProgramacion)
+                    .HasName("PK__Programa__74E652C048C1C33F");
+
+                entity.ToTable("Programacion");
+
+                entity.Property(e => e.IdProgramacion).ValueGeneratedNever();
+
+                entity.Property(e => e.FechaProgramacion).HasColumnType("datetime");
+
+                entity.Property(e => e.PeridoAcademico)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sala)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdAsignaturaNavigation)
+                    .WithMany(p => p.Programacions)
+                    .HasForeignKey(d => d.IdAsignatura)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Programacion_Asignatura");
+
+                entity.HasOne(d => d.IdProgramaNavigation)
+                    .WithMany(p => p.Programacions)
+                    .HasForeignKey(d => d.IdPrograma)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Programacion_Programa");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Programacions)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Programacion_Usuario");
+            });
+
+            modelBuilder.Entity<Rol>(entity =>
+            {
+                entity.HasKey(e => e.IdRol)
+                    .HasName("PK__Rol__2A49584CE3E5B5D0");
+
+                entity.ToTable("Rol");
+
+                entity.Property(e => e.IdRol).ValueGeneratedNever();
+
+                entity.Property(e => e.Rol1)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Rol");
+            });
+
+            modelBuilder.Entity<TipoHomologacion>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoHomologacion)
+                    .HasName("PK__TipoHomo__626070CDDA02B520");
+
+                entity.ToTable("TipoHomologacion");
+
+                entity.Property(e => e.IdTipoHomologacion).ValueGeneratedNever();
+
+                entity.Property(e => e.TipoHomologacion1)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("TipoHomologacion");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__tmp_ms_x__5B65BF97AC927F8D");
+
+                entity.ToTable("Usuario");
+
+                entity.Property(e => e.IdUsuario).ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NumeroDocumento)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrimerApellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrimerNombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SegundoApellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SegundoNombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdDocumentoNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdDocumento)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Usuario_Documento");
+
+                entity.HasOne(d => d.IdGeneroNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdGenero)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Usuario_Genero");
+
+                entity.HasOne(d => d.IdProgramaNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdPrograma)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Usuario_Programa");
+
+                entity.HasOne(d => d.IdRolNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdRol)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Usuario_Rol");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
